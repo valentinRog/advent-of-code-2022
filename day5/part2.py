1d6f8d85bc2c5b7ba69c6a6a9c958a57c7dad6de
valentinRog <valentin.rogister@gmail.com>
2022-12-06 13:06:49 +0100

    better day5

--- day5/part2.py
+++ day5/part2.py
@@ -1,21 +1,14 @@
 import sys
 
 data = sys.stdin.read().split("\n\n")
-
-game = [[] for _ in range(9)]
+game = [[] for _ in range(int(data[0].strip()[-1]))]
 for line in data[0].splitlines()[:-1]:
     for i, c in enumerate(line):
         if not ((i - 1) % 4) and c != " ":
             game[i // 4] = [c] + game[i // 4]
-
 for line in data[1].splitlines():
-    _, x, _, y, _, z = line.split();
-    game[int(z) - 1] += game[int(y) - 1][-int(x):]
-    for _ in range(int(x)):
-        if len(game[int(y) - 1]):
-            game[int(y) - 1].pop()
-
-for x in game:
-    if x:
-        print(x[-1], end="")
-print()
+    _, x, _, y, _, z = line.split()
+    x, y, z = map(int, [x, y, z])
+    game[z - 1] += game[y - 1][-x:]
+    game[y - 1] = game[y - 1][:-x]
+print("".join(e[-1] for e in game))

890c5d6a7d7d00c53ca9d0cef3ceb31ca24665b4
valentinRog <valentin.rogister@gmail.com>
2022-12-09 19:44:12 +0100

    better day8

--- day8/part1.py
+++ day8/part1.py
@@ -4,29 +4,15 @@ data = list(map(lambda x: list(map(lambda y: int(y), list(x))), sys.stdin.read()
 s = set()
 N = len(data)
 
-for j in range(N):
-    prev_max = -1
-    for i in range(N):
-        if data[i][j] > prev_max:
-            s.add((i, j))
-            prev_max = data[i][j]
-for j in range(N):
-    prev_max = -1
-    for i in reversed(range(N)):
-        if data[i][j] > prev_max:
-            s.add((i, j))
-            prev_max = data[i][j]
-for i in range(N):
-    prev_max = -1
-    for j in range(N):
-        if data[i][j] > prev_max:
-            s.add((i, j))
-            prev_max = data[i][j]
-for i in range(N):
-    prev_max = -1
-    for j in reversed(range(N)):
-        if data[i][j] > prev_max:
-            s.add((i, j))
-            prev_max = data[i][j]
-        
+NORTH = [[(i, j) for i in range(N)] for j in range(N)]
+SOUTH = [[(N - 1 - i, j) for i in range(N)] for j in range(N)]
+WEST = [[(i, j) for j in range(N)] for i in range(N)]
+EST =[[(i, N - 1 - j) for j in range(N)] for i in range(N)]
+
+for x in NORTH + SOUTH + WEST + EST:
+    prev = -1
+    for p in x:
+        if data[p[0]][p[1]] > prev:
+            prev = data[p[0]][p[1]]
+            s.add(p)
 print(len(s))

0d8e836dc7d30b1eea75595cb62aef08aab91f82
valentinRog <valentin.rogister@gmail.com>
2022-12-09 19:48:53 +0100

    better day8

--- day8/part1.py
+++ day8/part1.py
@@ -1,15 +1,16 @@
 import sys
+from itertools import chain
 
 data = list(map(lambda x: list(map(lambda y: int(y), list(x))), sys.stdin.read().splitlines()))
 s = set()
 N = len(data)
 
-NORTH = [[(i, j) for i in range(N)] for j in range(N)]
-SOUTH = [[(N - 1 - i, j) for i in range(N)] for j in range(N)]
-WEST = [[(i, j) for j in range(N)] for i in range(N)]
-EST =[[(i, N - 1 - j) for j in range(N)] for i in range(N)]
+NORTH = ([(i, j) for i in range(N)] for j in range(N))
+SOUTH = ([(N - 1 - i, j) for i in range(N)] for j in range(N))
+WEST = ([(i, j) for j in range(N)] for i in range(N))
+EST = ([(i, N - 1 - j) for j in range(N)] for i in range(N))
 
-for x in NORTH + SOUTH + WEST + EST:
+for x in chain(NORTH, SOUTH, WEST, EST):
     prev = -1
     for p in x:
         if data[p[0]][p[1]] > prev:

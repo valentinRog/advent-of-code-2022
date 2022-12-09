8672cb85bdcb724ae89c20b45b3daf9da47db193
valentinRog <valentin.rogister@gmail.com>
2022-12-09 21:09:46 +0100

    perfect day8 part2

--- day8/part2.py
+++ day8/part2.py
@@ -1,17 +1,20 @@
 import sys
 from functools import reduce
+from itertools import takewhile
 
 data = list(map(lambda x: list(map(lambda y: int(y), list(x))), sys.stdin.read().splitlines()))
 N = len(data)
 
+def north(i, j): return data[i][:j + 1][::-1]
+def south(i, j): return data[i][j:]
+def west(i, j): return [data[k][j] for k in reversed(range(i + 1))]
+def east(i, j): return [data[k][j] for k in range(i, N)]
+
 def score(i, j):
-    arr = [data[i][:j + 1][::-1], data[i][j:], [data[k][j] for k in reversed(range(i + 1))], [data[k][j] for k in range(i, N)]]
     scores = []
-    for arr in arr:
-        res = 0
-        for x in arr[1:]:
-            res += 1
-            if x >= arr[0]: break
+    for arr in [north(i, j), south(i, j), west(i, j), east(i, j)]:
+        res = len(tuple(takewhile(lambda x: x < arr[0], arr[1:])))
+        res += res < len(arr[1:])
         scores.append(res)
     return reduce(lambda x, y: x * y, scores)
 

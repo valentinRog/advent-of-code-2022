5a01db7ddabc6e6db72f5f412388bee7eb1fbb12
valentinRog <valentin.rogister@gmail.com>
2022-12-10 15:57:07 +0100

    better day01

--- day01/part2.py
+++ day01/part2.py
@@ -1,4 +1,4 @@
 import sys
 
-data = [[int(x) for x in filter(None, i.split("\n"))] for i in sys.stdin.read().split("\n\n")]
-print(sum(sorted(map(lambda x: sum(x), data))[-3:]))
+data = [[int(x) for x in i.split("\n")] for i in sys.stdin.read().strip().split("\n\n")]
+print(sum(sorted(map(sum, data))[-3:]))

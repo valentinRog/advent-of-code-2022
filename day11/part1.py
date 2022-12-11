98341d50a9b1b2b0b3bc9ded6a731364ce340ba1
valentinRog <valentin.rogister@gmail.com>
2022-12-11 15:52:31 +0100

    cleaner day11

--- day11/part1.py
+++ day11/part1.py
@@ -1,27 +1,25 @@
 import sys
-from functools import reduce
+from math import prod
 
-monkeys = []
 
 def get_op(line):
-    operation = line.split(":")[1].split()
-    if operation[-1] == "old":
-        return lambda x: x**2
-    elif operation[-2] == "+":
-        return lambda x: x + int(operation[-1])
-    return lambda x: x * int(operation[-1])
+    o, v = line.split(":")[1].split()[-2:]
+    return {
+        "+": lambda x: x + int(v),
+        "*": lambda x: x * int(v),
+    }[o] if v != "old" else lambda x: x ** 2
 
-for monkey in sys.stdin.read().split("\n\n"):
-    arr = monkey.splitlines()
-    monkey = {
-        "items": list(map(lambda x: int(x.strip()), arr[1].split(":")[1].split(","))),
-        "operation": get_op(arr[2]),
-        "test": int(arr[3].split()[-1]),
-        "true": int(arr[4].split()[-1]),
-        "false": int(arr[5].split()[-1]),
+monkeys = [
+    {
+        "items": list(map(lambda x: int(x.strip()), monkey[1].split(":")[1].split(","))),
+        "operation": get_op(monkey[2]),
+        "test": int(monkey[3].split()[-1]),
+        "true": int(monkey[4].split()[-1]),
+        "false": int(monkey[5].split()[-1]),
         "inspections": 0,
     }
-    monkeys.append(monkey)
+    for monkey in map(str.splitlines, sys.stdin.read().split("\n\n"))
+]
 
 for _ in range(20):
     for monkey in monkeys:
@@ -31,7 +29,7 @@ for _ in range(20):
                 monkeys[monkey["true"]]["items"].append(worry)
             else:
                 monkeys[monkey["false"]]["items"].append(worry)
-            monkey["inspections"] += 1
+        monkey["inspections"] += len(monkey["items"])
         monkey["items"] = []
 
-print(reduce(lambda x, y: x * y, sorted([monkey["inspections"] for monkey in monkeys])[-2:]))
+print(prod(sorted([monkey["inspections"] for monkey in monkeys])[-2:]))

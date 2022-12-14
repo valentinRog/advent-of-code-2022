c584ec5c1c35dfcc3bce87e9203e560b3de3d5b4
valentinRog <valentin.rogister@gmail.com>
2022-12-14 10:40:53 +0100

    day14 part1

--- day14/part1.py
+++ day14/part1.py
@@ -5,10 +5,10 @@ data = [
      for line in sys.stdin.read().strip().splitlines()
 ]
 
-X_MIN = min(min([p[0] for p in line]) for line in data)
-X_MAX = max(max([p[0] for p in line]) for line in data)
-Y_MIN = min(min([p[1] for p in line]) for line in data)
-Y_MAX = max(max([p[1] for p in line]) for line in data)
+X_MIN = min(min([p[0] for p in line]) for line in data) - 30
+X_MAX = max(max([p[0] for p in line]) for line in data) + 30
+Y_MIN = min(min([p[1] for p in line]) for line in data) - 30
+Y_MAX = max(max([p[1] for p in line]) for line in data) + 30
 
 W = X_MAX - X_MIN + 1
 H = Y_MAX - Y_MIN + 1
@@ -35,3 +35,23 @@ for line in data:
             m[y][px1] = "#"
             
 print_m()
+res = 0
+while True:
+    sp = [500 - X_MIN, 0]
+    while sp[1] != H - 1:
+        sp[1] += 1
+        if m[sp[1]][sp[0]] == ".":
+            continue
+        elif sp[0] > 0 and m[sp[1]][sp[0] - 1] == ".":
+            sp[0] -= 1
+        elif sp[0] < W - 1 and m[sp[1]][sp[0] + 1] == ".":
+            sp[0] += 1
+        else:
+            sp[1] -= 1
+            break
+    if sp[1] == H - 1:
+        break
+    res += 1
+    m[sp[1]][sp[0]] = "o"
+    print_m()
+print(res)

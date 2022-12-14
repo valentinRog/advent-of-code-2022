09d1e27f6fe58e448a0e058c2bc0bc9d2bc64fe1
valentinRog <valentin.rogister@gmail.com>
2022-12-14 12:05:57 +0100

    -1 line in day 13

--- day13/part1.py
+++ day13/part1.py
@@ -9,6 +9,5 @@ def cmp(a, b):
     if int == type(a) == type(b):
       return a - b
     a, b = map(lambda x: [x] if type(x) == int else x, (a, b))
-    d = next((e for e in zip(a, b) if cmp(*e)), None)
-    return cmp(*d) if d is not None else len(a) - len(b)
+    return next((d for d in (cmp(*e) for e in zip(a, b)) if d), len(a) - len(b))
 print(sum([i + 1 for i, e in enumerate(data) if cmp(*e) <= 0]))
--- day13/part2.py
+++ day13/part2.py
@@ -7,8 +7,7 @@ def cmp(a, b):
     if int == type(a) == type(b):
       return a - b
     a, b = map(lambda x: [x] if type(x) == int else x, (a, b))
-    d = next((e for e in zip(a, b) if cmp(*e)), None)
-    return cmp(*d) if d is not None else len(a) - len(b)
+    return next((d for d in (cmp(*e) for e in zip(a, b)) if d), len(a) - len(b))
 
 data = sorted(data + [[[2]], [[6]]], key=cmp_to_key(cmp))
 print((data.index([[2]]) + 1) * (data.index([[6]]) + 1))

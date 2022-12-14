5c4dc58602b1ce3a404d26d02aa8bff3c05e4f80
valentinRog <valentin.rogister@gmail.com>
2022-12-14 12:06:55 +0100

    new line after fn

--- day13/part1.py
+++ day13/part1.py
@@ -10,4 +10,5 @@ def cmp(a, b):
       return a - b
     a, b = map(lambda x: [x] if type(x) == int else x, (a, b))
     return next((d for d in (cmp(*e) for e in zip(a, b)) if d), len(a) - len(b))
+
 print(sum([i + 1 for i, e in enumerate(data) if cmp(*e) <= 0]))

e74fc11260fb9d70c0db445c89d4ae1084a75f36
valentinRog <valentin.rogister@gmail.com>
2022-12-14 12:52:28 +0100

    cleaner day14 (still not perfect)

--- day14/part1.py
+++ day14/part1.py
@@ -1,57 +1,43 @@
 import sys
+from itertools import count
 
 data = [
     [tuple(map(int, p.split(","))) for p in line.split(" -> ")]
      for line in sys.stdin.read().strip().splitlines()
 ]
 
-X_MIN = min(min([p[0] for p in line]) for line in data) - 30
-X_MAX = max(max([p[0] for p in line]) for line in data) + 30
-Y_MIN = min(min([p[1] for p in line]) for line in data) - 30
-Y_MAX = max(max([p[1] for p in line]) for line in data) + 30
+Y_MAX = max(max([p[1] for p in line]) for line in data)
 
-W = X_MAX - X_MIN + 1
-H = Y_MAX - Y_MIN + 1
-
-m = [["." for _ in range(W)] for _ in range(H)]
-
-def print_m():
-    for y in range(H):
-        for x in range(W):
-            print(m[y][x], end="")
-        print()
-
+s = set()
 for line in data:
     for i in range(len(line) - 1):
         px = sorted(line[i:i+2], key=lambda e: e[0])
-        px1 = px[0][0] - X_MIN
-        px2 = px[1][0] - X_MIN
+        px1 = px[0][0]
+        px2 = px[1][0]
         py = sorted(line[i:i+2], key=lambda e: e[1])
-        py1 = py[0][1] - Y_MIN
-        py2 = py[1][1] - Y_MIN
+        py1 = py[0][1]
+        py2 = py[1][1]
         for x in range(px1, px2 + 1):
-            m[py1][x] = "#"
+            s.add((x, py1))
         for y in range(py1, py2 + 1):
-            m[y][px1] = "#"
-            
-print_m()
-res = 0
+            s.add((px1, y))
+
+n = 0
 while True:
-    sp = [500 - X_MIN, 0]
-    while sp[1] != H - 1:
+    sp = [500, 0]
+    while sp[1] != Y_MAX:
         sp[1] += 1
-        if m[sp[1]][sp[0]] == ".":
+        if tuple(sp) not in s:
             continue
-        elif sp[0] > 0 and m[sp[1]][sp[0] - 1] == ".":
+        elif (sp[0] - 1, sp[1]) not in s:
             sp[0] -= 1
-        elif sp[0] < W - 1 and m[sp[1]][sp[0] + 1] == ".":
+        elif (sp[0] + 1, sp[1]) not in s:
             sp[0] += 1
         else:
             sp[1] -= 1
             break
-    if sp[1] == H - 1:
+    if sp[1] == Y_MAX:
         break
-    res += 1
-    m[sp[1]][sp[0]] = "o"
-    print_m()
-print(res)
+    s.add(tuple(sp))
+    n += 1
+print(n)
--- day14/part2.py
+++ day14/part2.py
@@ -1,67 +1,46 @@
 import sys
+from itertools import count
 
 data = [
     [tuple(map(int, p.split(","))) for p in line.split(" -> ")]
      for line in sys.stdin.read().strip().splitlines()
 ]
 
-X_MIN = min(min([p[0] for p in line]) for line in data) - 1000
-X_MAX = max(max([p[0] for p in line]) for line in data) + 1000
-Y_MIN = 0
-Y_MAX = max(max([p[1] for p in line]) for line in data) + 50
+Y_MAX = max(max([p[1] for p in line]) for line in data)
 
-W = X_MAX - X_MIN + 1
-H = Y_MAX - Y_MIN + 1
-
-m = [["." for _ in range(W)] for _ in range(H)]
-
-for x in range(W):
-    y = max(max([p[1] for p in line]) for line in data) - Y_MIN + 2
-    m[y][x] = "x"
-
-def print_m():
-    for y in range(H):
-        for x in range(W):
-            if [x, y] == [500 - X_MIN, 0]:
-                print("S", end="")
-            else:
-                print(m[y][x], end="")
-        print()
-
+s = set()
 for line in data:
     for i in range(len(line) - 1):
         px = sorted(line[i:i+2], key=lambda e: e[0])
-        px1 = px[0][0] - X_MIN
-        px2 = px[1][0] - X_MIN
+        px1 = px[0][0]
+        px2 = px[1][0]
         py = sorted(line[i:i+2], key=lambda e: e[1])
-        py1 = py[0][1] - Y_MIN
-        py2 = py[1][1] - Y_MIN
+        py1 = py[0][1]
+        py2 = py[1][1]
         for x in range(px1, px2 + 1):
-            m[py1][x] = "#"
+            s.add((x, py1))
         for y in range(py1, py2 + 1):
-            m[y][px1] = "#"
+            s.add((px1, y))
 
-       
-       
-res = 0
+n = 0
 while True:
-    sp = [500 - X_MIN, 0]
-    yo = 0
-    while sp != [500 - X_MIN, 0] or not yo:
+    sp = [500, 0]
+    while (500, 0) not in s:
         sp[1] += 1
-        yo = 1
-        if m[sp[1]][sp[0]] == ".":
+        s.add((sp[0], Y_MAX + 2))
+        s.add((sp[0] - 1, Y_MAX + 2))
+        s.add((sp[0] + 1, Y_MAX + 2))
+        if tuple(sp) not in s:
             continue
-        elif m[sp[1]][sp[0] - 1] == ".":
+        elif (sp[0] - 1, sp[1]) not in s:
             sp[0] -= 1
-        elif m[sp[1]][sp[0] + 1] == ".":
+        elif (sp[0] + 1, sp[1]) not in s:
             sp[0] += 1
         else:
             sp[1] -= 1
             break
-    res += 1
-    if sp == [500 - X_MIN, 0]:
+    if (500, 0) in s:
         break
-    m[sp[1]][sp[0]] = "o"
-print_m()
-print(res)
+    s.add(tuple(sp))
+    n += 1
+print(n)

22a8b7b19119740845285501034b9f264550bcb5
valentinRog <valentin.rogister@gmail.com>
2022-12-15 22:58:44 +0100

    created a supercool algorithm by accident and have no idea how it works

--- day15/part2.py
+++ day15/part2.py
@@ -5,7 +5,6 @@ ROW = 4000000
 
 data = []
 
-
 def dist(p1, p2):
     return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
 
@@ -20,22 +19,24 @@ for line in sys.stdin.read().strip().splitlines():
 def addt(t1, t2):
     return tuple(sum(e) for e in zip(t1, t2))
 
+def xjump(p):
+    start = p[0]
+    b, d = next(((line[0], line[2]) for line in data if dist(p, line[0]) <= line[2]))
+    return b[0] - p[0] + d - abs(p[1] - b[1]) + 1
+
+def dist_to_border_max(p):
+    return max([abs(d - line[2]) for line in data if (d := dist(p, line[0])) <= line[2]])
 
 y = 0
 while y < ROW:
     print(y, end="\r")
     x = 0
-    jumpy = -1
+    arr = []
     while x < ROW:
-        jump = 1
-        stop = True
-        ds = [abs(d - line[2]) for line in data if (d := dist((x, y), line[0])) <= line[2]]
-        if not len(ds):
-            print((x, y))
-            print(4000000 * x + y)
+        try:
+            arr.append(dist_to_border_max((x, y)))
+            x += xjump((x, y))
+        except:
+            print(4_000_000 * x + y)
             exit()
-        x += max(ds) or 1
-        jump = max(ds) or 1
-        if jumpy == -1 or jump < jumpy:
-            jumpy = jump
-    y += jumpy >> 1 or 1
+    y += min(arr) >> 1 or 1

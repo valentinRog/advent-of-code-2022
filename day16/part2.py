369106bf90eb2e0522dc9cc4d4ad552508d75449
valentinRog <valentin.rogister@gmail.com>
2022-12-16 16:48:23 +0100

    working part2

--- day16/part2.py
+++ day16/part2.py
@@ -14,13 +14,25 @@ data = {
 def best(t=0, valve="AA", opened=None):
     if opened is None:
         opened = tuple()
-    if t == 30:
-        return 0
+    if t == 26:
+        return 0, opened
     score = sum([data[e]["rate"] for e in opened])
     s1 = 0
+    back1 = ()
     if data[valve]["rate"] and valve not in opened:
-        s1 = best(t + 1, valve, tuple(sorted(opened + (valve,))))
-    s2 = max(best(t + 1, v, opened) for v in data[valve]["nb"])
-    return score + max(s1, s2)
-  
-print(best())
+        s1, back1 = best(t + 1, valve, tuple(opened + (valve,)))
+    s2, back2 = max((best(t + 1, v, opened) for v in data[valve]["nb"]), key=lambda x: x[0])
+    if s1 > s2:
+        return score + s1, back1
+    return score + s2, back2
+
+score, opened = best()
+print(score)
+print(opened)
+
+for e in opened:
+    data[e]["rate"] = 0
+best.cache_clear()
+score2, opened = best()
+print(opened)
+print(score + score2)

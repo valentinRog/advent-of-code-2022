ad24e16878542ac4b2f0a83d15406d3969e3604a
valentinRog <valentin.rogister@gmail.com>
2022-12-17 04:56:23 +0100

    more accurate and general solution for part2

--- day16/part2.py
+++ day16/part2.py
@@ -10,24 +10,73 @@ data = {
     for line in sys.stdin.read().strip().splitlines()
 }
 
-@functools.cache
-def best(t=0, valve="AA", opened=None):
-    if opened is None:
-        opened = tuple()
-    if t == 26:
-        return 0, opened
-    score = sum([data[e]["rate"] for e in opened])
-    s1 = 0
-    back1 = ()
-    if data[valve]["rate"] and valve not in opened:
-        s1, back1 = best(t + 1, valve, tuple(opened + (valve,)))
-    s2, back2 = max((best(t + 1, v, opened) for v in data[valve]["nb"]), key=lambda x: x[0])
-    if s1 > s2:
-        return score + s1, back1
-    return score + s2, back2
+TICK = 26
 
-score, opened = best()
-for e in opened:
-    data[e]["rate"] = 0
-best.cache_clear()
-print(score + best()[0])
+def dist(a, b, s=None):
+    if s is None:
+        s = tuple()
+    if a == b:
+        return 0
+    return 1 + min((dist(x, b, tuple(s + (a,))) for x in data[a]["nb"] if x not in s), default=100)
+
+
+for k in data.keys():
+    data[k]["dist"] = {
+        o: dist(k, o)
+        for o in data.keys()
+    }
+
+keys = list(filter(lambda x: data[x]["rate"] > 0, data.keys()))
+path = []
+arr = [(0, ["AA"])]
+while len(arr):
+    new_arr = []
+    for e in arr:
+        for k in keys:
+            if k not in e[1]:
+                tmp = e[1] + [k]
+                cost = data[tmp[-2]]["dist"][tmp[-1]] + 1
+                path.append(tmp)
+                if e[0] + cost <= TICK or len(tmp) == len(keys) + 1:
+                    path.append(tmp)
+                if e[0] + cost < TICK and len(tmp) != len(keys) + 1:
+                    new_arr.append((e[0] + cost, tmp))
+    arr = list(new_arr)
+
+def score(p):
+    rates = [0]
+    res = 0
+    d = data[p[0]]["dist"][p[1]]
+    for tick in range(TICK):
+        res += sum(rates)
+        if d:
+            d -= 1
+        else:
+            p = p[1:]
+            d = data[p[0]]["dist"][p[1]] if len(p) > 1 else 0
+            if len(p):
+                rates.append(data[p[0]]["rate"])
+    return res
+
+print(len(path))
+
+bitset = []
+
+path = sorted(path, key=functools.cmp_to_key(lambda x, y: score(x) - score(y)), reverse=True)
+
+for e in path:
+    n = 0
+    for v in e[1:]:
+        n += 1 << keys.index(v)
+    bitset.append(n)
+
+scores = [score(p) for p in path]
+
+res = 0
+for i1 in range(len(path)):
+    for i2 in range(len(path)):
+        if not (bitset[i1] & bitset[i2]):
+            if (x := scores[i1] + scores[i2]) > res:
+                res = x
+            break
+print(res)

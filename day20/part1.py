c8281f660ab40467f37aaab86a749440656d8f69
valentinRog <valentin.rogister@gmail.com>
2022-12-20 19:46:53 +0100

    day20 part1

---
+++ day20/part1.py
@@ -0,0 +1,37 @@
+import sys
+from collections import deque
+
+data = list(map(int , sys.stdin.read().split()))
+
+arr = deque((e[0], e[1]) for e in enumerate(data))
+
+def r():
+    global arr
+    e = arr[0]
+    arr.popleft()
+    arr.append(e)
+
+def align(e):
+    while arr[0] != e:
+        r()
+
+for e in enumerate(data):
+    i1 = arr.index(e)
+    a = e[1]
+    align(e)
+    arr.popleft()
+    if a < 0:
+        a += (-a // len(arr)) * len(arr)
+    a %= len(arr)
+    for _ in range(a):
+        r()
+    arr.appendleft(e)
+
+res = 0
+for n in (1000, 2000, 3000):
+    while arr[0][1]:
+        r()
+    for _ in range(n):
+        r()
+    res += arr[0][1]
+print(res)

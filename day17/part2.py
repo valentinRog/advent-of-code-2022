509c9647c31c89a9e3c1407141610cabe835c057
valentinRog <valentin.rogister@gmail.com>
2022-12-20 12:45:44 +0100

    I was stupid the whole time

--- day17/part2.py
+++ day17/part2.py
@@ -3,7 +3,7 @@ import itertools
 
 data = sys.stdin.read().strip()
 
-N_ROCK = 1365 + 2080
+N_ROCK = int(1e12)
 
 rocks = [
     {(0, 0), (1, 0), (2, 0), (3, 0)},
@@ -44,34 +44,58 @@ def right(rock):
 
 def minimize():
     conf = tuple(max((p[1] for p in s if p[0] == x), default=0) for x in range(7))
-    h_min = min(conf) - 5
+    h_min = min(conf) - 30
     return set((p for p in s if p[1] > h_min)) if h_min > 0 else s
 
 def hg():
     return max((p[1] for p in s), default=0) + 1
+
+def top():
+    res = []
+    for x in range(7):
+        res.append(max((p[1] for p in s if p[0] == x), default=0))
+    y_min = min(res)
+    res = map(lambda x: x - y_min, res)
+    return tuple(res)
 
 move = {
     "<": left,
     ">": right
 }
 
-def drop(rock, it):
+def drop(rock, it, i2):
     rock = {addt(p, falling0()) for p in rock}
     while True:
         dir = next(it)
+        i2 +=1
+        i2 %= len(data)
         rock = move[dir](rock)
         try:
             rock = down(rock)
         except:
             break
-    return rock
+    return rock, i2
 
 
 it = itertools.cycle(iter(data))
 it_rock = itertools.cycle(iter(rocks))
 i = N_ROCK
+i1 = i2 = 0
+conf = dict()
+h = 0
 while i:
-    s |= drop(next(it_rock), it)
+    rock, i2 = drop(next(it_rock), it, i2)
+    s |= rock
     s = minimize()
+    if not h:
+        if tuple(top() + (i1, i2)) in conf.keys():
+            e = conf[tuple(top() + (i1, i2))]
+            di = abs(i - e[0])
+            dh = abs(hg() - e[1])
+            h = (i // di) * dh
+            i %= di
+        conf[tuple(top() + (i1, i2))] = (i, hg())
     i -= 1
-print(hg())
+    i1 +=1
+    i1 %= len(rocks)
+print(hg() + h)

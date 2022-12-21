68a123ce8193fae39b372ea8578c2eff88e24e31
valentinRog <valentin.rogister@gmail.com>
2022-12-21 15:06:41 +0100

    better day19 but still ugly

--- day19/part2.py
+++ day19/part2.py
@@ -11,129 +11,104 @@ data = [
     for line in sys.stdin.read().strip().splitlines()
 ]
 
-
-@functools.cache
 def best(
-        ore_ore,
-        clay_ore,
-        obsidian_ore,
-        obsidian_clay,
-        geode_ore,
-        geode_obsidian,
-        ore=0,
-        clay=0,
-        obsidian=0,
-        geode=0,
-        ore_rob=1,
-        clay_rob=0,
-        obsidian_rob=0,
-        geode_rob=0,
-        t=0):
+    ore_ore,
+    clay_ore,
+    obsidian_ore,
+    obsidian_clay,
+    geode_ore,
+    geode_obsidian
+):
 
-    if t == 32:
-        return geode
-    arr = []
+    @functools.cache
+    def compute(
+            ore=0,
+            clay=0,
+            obsidian=0,
+            geode=0,
+            ore_rob=1,
+            clay_rob=0,
+            obsidian_rob=0,
+            geode_rob=0,
+            t=0
+    ):
 
-    if ore >= geode_ore and obsidian >= geode_obsidian:
-        return best(
-            ore_ore,
-            clay_ore,
-            obsidian_ore,
-            obsidian_clay,
-            geode_ore,
-            geode_obsidian,
-            ore + ore_rob - geode_ore,
-            clay + clay_rob,
-            obsidian + obsidian_rob - geode_obsidian,
-            geode + geode_rob,
-            ore_rob,
-            clay_rob,
-            obsidian_rob,
-            geode_rob + 1,
-            t + 1
-        )
+        if t == 32:
+            return geode
+        arr = []
 
-    if ore >= ore_ore:
-        arr.append(
-            best(
-                ore_ore,
-                clay_ore,
-                obsidian_ore,
-                obsidian_clay,
-                geode_ore,
-                geode_obsidian,
-                ore + ore_rob - ore_ore,
+        if ore >= geode_ore and obsidian >= geode_obsidian:
+            return compute(
+                ore + ore_rob - geode_ore,
                 clay + clay_rob,
-                obsidian + obsidian_rob,
-                geode + geode_rob,
-                ore_rob + 1,
-                clay_rob,
-                obsidian_rob,
-                geode_rob,
-                t + 1)
-        )
-
-    if ore >= clay_ore:
-        arr.append(
-            best(
-                ore_ore,
-                clay_ore,
-                obsidian_ore,
-                obsidian_clay,
-                geode_ore,
-                geode_obsidian,
-                ore + ore_rob - clay_ore,
-                clay + clay_rob,
-                obsidian + obsidian_rob,
-                geode + geode_rob,
-                ore_rob,
-                clay_rob + 1,
-                obsidian_rob,
-                geode_rob,
-                t + 1)
-        )
-
-    if ore >= obsidian_ore and clay >= obsidian_clay:
-        arr.append(
-            best(
-                ore_ore,
-                clay_ore,
-                obsidian_ore,
-                obsidian_clay,
-                geode_ore,
-                geode_obsidian,
-                ore + ore_rob - obsidian_ore,
-                clay + clay_rob - obsidian_clay,
-                obsidian + obsidian_rob,
-                geode + geode_rob,
-                ore_rob,
-                clay_rob,
-                obsidian_rob + 1,
-                geode_rob,
-                t + 1)
-        )
-
-    if ore < ore_ore:
-        arr.append(
-            best(
-                ore_ore,
-                clay_ore,
-                obsidian_ore,
-                obsidian_clay,
-                geode_ore,
-                geode_obsidian,
-                ore + ore_rob,
-                clay + clay_rob,
-                obsidian + obsidian_rob,
+                obsidian + obsidian_rob - geode_obsidian,
                 geode + geode_rob,
                 ore_rob,
                 clay_rob,
                 obsidian_rob,
-                geode_rob,
+                geode_rob + 1,
                 t + 1
             )
-        )
-    return max(arr)
+
+        if ore >= ore_ore:
+            arr.append(
+                compute(
+                    ore + ore_rob - ore_ore,
+                    clay + clay_rob,
+                    obsidian + obsidian_rob,
+                    geode + geode_rob,
+                    ore_rob + 1,
+                    clay_rob,
+                    obsidian_rob,
+                    geode_rob,
+                    t + 1)
+            )
+
+        if ore >= clay_ore:
+            arr.append(
+                compute(
+                    ore + ore_rob - clay_ore,
+                    clay + clay_rob,
+                    obsidian + obsidian_rob,
+                    geode + geode_rob,
+                    ore_rob,
+                    clay_rob + 1,
+                    obsidian_rob,
+                    geode_rob,
+                    t + 1)
+            )
+
+        if ore >= obsidian_ore and clay >= obsidian_clay:
+            arr.append(
+                compute(
+                    ore + ore_rob - obsidian_ore,
+                    clay + clay_rob - obsidian_clay,
+                    obsidian + obsidian_rob,
+                    geode + geode_rob,
+                    ore_rob,
+                    clay_rob,
+                    obsidian_rob + 1,
+                    geode_rob,
+                    t + 1)
+            )
+
+        if ore < ore_ore:
+            arr.append(
+                compute(
+                    ore + ore_rob,
+                    clay + clay_rob,
+                    obsidian + obsidian_rob,
+                    geode + geode_rob,
+                    ore_rob,
+                    clay_rob,
+                    obsidian_rob,
+                    geode_rob,
+                    t + 1
+                )
+            )
+        return max(arr)
+
+    return compute
 
 
 res = 1
@@ -145,8 +120,7 @@ for i, line in enumerate(data, 1):
         line["obsidian"][1],
         line["geode"][0],
         line["geode"][1],
-    )
-    best.cache_clear()
+    )()
     if i == 3:
         break
 print(res)

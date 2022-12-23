2f1b8c6dd07995340df0a365ab4330ff4592e6b4
valentinRog <valentin.rogister@gmail.com>
2022-12-23 17:15:25 +0100

    better day23

--- day23/part1.py
+++ day23/part1.py
@@ -1,24 +1,12 @@
 import sys
-from collections import deque
+import collections
+from itertools import chain
 
-s = set()
-
-for y, line in enumerate(sys.stdin.read().splitlines()):
-    for x, c in enumerate(line):
-        if c == "#":
-            s.add(complex(x, y))
-
-
-def print_conf():
-    for y in range(min(map(lambda z: int(z.imag), s)), max(map(lambda z: int(z.imag), s)) + 1):
-        for x in range(min(map(lambda z: int(z.real), s)), max(map(lambda z: int(z.real), s)) + 1):
-            if complex(x, y) in s:
-                print("#", end="")
-            else:
-                print(".", end="")
-        print()
-    print()
-
+s = {
+    complex(x, y)
+    for y, line in enumerate(sys.stdin.read().splitlines())
+    for x, c in enumerate(line) if c == "#"
+}
 
 N = -1j
 S = 1j
@@ -32,39 +20,29 @@ dst = {
     E: (E, N + E, S + E),
 }
 
-q = [N, S, W, E]
-
-
-def isfree(e):
-    for v in dst.values():
-        for d in v:
-            if e + d in s:
-                return False
-    return True
-
+q = collections.deque([N, S, W, E])
 
 for _ in range(10):
     nps = dict()
+    c = collections.Counter()
     for e in s:
-        if isfree(e):
+        if all(e + d not in s for d in chain.from_iterable(v for v in dst.values())):
             nps[e] = e
         else:
             for d in q:
-                if e + dst[d][0] not in s and e + dst[d][1] not in s and e + dst[d][2] not in s:
+                if all(e + p not in s for p in dst[d]):
                     nps[e] = e + d
+                    c.update((e + d,))
                     break
                 else:
                     nps[e] = e
-    for e in set(s):
-        if list(nps.values()).count(nps[e]) == 1:
-            s.remove(e)
-            s.add(nps[e])
-    print_conf()
-    q = q[1:] + [q[0]]
-    
-res = 0
-for y in range(min(map(lambda z: int(z.imag), s)), max(map(lambda z: int(z.imag), s)) + 1):
-    for x in range(min(map(lambda z: int(z.real), s)), max(map(lambda z: int(z.real), s)) + 1):
-        if complex(x, y) not in s:
-            res += 1
-print(res)
+    s = {nps[e] if c[nps[e]] == 1 else e for e in s}
+    q.append(q.popleft())
+
+sr = set(map(lambda z: int(z.real), s))
+si = set(map(lambda z: int(z.imag), s))
+print(sum(
+    complex(x, y) not in s
+    for y in range(min(si), max(si) + 1)
+    for x in range(min(sr), max(sr) + 1)
+))
--- day23/part2.py
+++ day23/part2.py
@@ -1,25 +1,13 @@
 import sys
-from collections import deque
+import collections
 import itertools
+from itertools import chain
 
-s = set()
-
-for y, line in enumerate(sys.stdin.read().splitlines()):
-    for x, c in enumerate(line):
-        if c == "#":
-            s.add(complex(x, y))
-
-
-def print_conf():
-    for y in range(min(map(lambda z: int(z.imag), s)), max(map(lambda z: int(z.imag), s)) + 1):
-        for x in range(min(map(lambda z: int(z.real), s)), max(map(lambda z: int(z.real), s)) + 1):
-            if complex(x, y) in s:
-                print("#", end="")
-            else:
-                print(".", end="")
-        print()
-    print()
-
+s = {
+    complex(x, y)
+    for y, line in enumerate(sys.stdin.read().splitlines())
+    for x, c in enumerate(line) if c == "#"
+}
 
 N = -1j
 S = 1j
@@ -33,40 +21,24 @@ dst = {
     E: (E, N + E, S + E),
 }
 
-q = [N, S, W, E]
-
-
-def isfree(e):
-    for v in dst.values():
-        for d in v:
-            if e + d in s:
-                return False
-    return True
-
-def stuck(nps):
-    for k, v in nps.items():
-        if k != v:
-            return False
-    return True
+q = collections.deque([N, S, W, E])
 
 for i in itertools.count():
     nps = dict()
+    c = collections.Counter()
     for e in s:
-        if isfree(e):
+        if all(e + d not in s for d in chain.from_iterable(v for v in dst.values())):
             nps[e] = e
         else:
             for d in q:
-                if e + dst[d][0] not in s and e + dst[d][1] not in s and e + dst[d][2] not in s:
+                if all(e + p not in s for p in dst[d]):
                     nps[e] = e + d
+                    c.update((e + d,))
                     break
                 else:
                     nps[e] = e
-    if stuck(nps):
+    if all(k == v for k, v in nps.items()):
         print(i + 1)
         break
-    for e in set(s):
-        if list(nps.values()).count(nps[e]) == 1:
-            s.remove(e)
-            s.add(nps[e])
-    q = q[1:] + [q[0]]
-    
+    s = {nps[e] if c[nps[e]] == 1 else e for e in s}
+    q.append(q.popleft())

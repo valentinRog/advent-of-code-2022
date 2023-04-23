9617c98c14098b5f626a001657cd8e345322f284
Valentin Rogister <valentin.rogister@gmail.com>
2023-04-23 17:03:25 +0200

    format

--- day16/part1.py
+++ day16/part1.py
@@ -5,7 +5,7 @@ import collections
 data = {
     line.split()[1]: {
         "rate": int(line.split()[4].split("=")[-1][:-1]),
-        "nb": [nb.replace(",", "") for nb in line.split("to")[-1].split()[1:]]
+        "nb": [nb.replace(",", "") for nb in line.split("to")[-1].split()[1:]],
     }
     for line in sys.stdin.read().strip().splitlines()
 }
@@ -25,7 +25,6 @@ def dist(a, b):
 
 
 valves = [k for k, v in data.items() if v["rate"]]
-
 T = 30
 
 
@@ -33,14 +32,11 @@ T = 30
 def dfs(t=0, valve="AA", opened=0):
     if t == T:
         return 0
-    score = sum(
-        data[e]["rate"]
-        for i, e in enumerate(valves) if opened & 1 << i
-    )
-    res = (T-t)*score
+    score = sum(data[e]["rate"] for i, e in enumerate(valves) if opened & 1 << i)
+    res = (T - t) * score
     for v in filter(lambda x: not opened & 1 << x, range(len(valves))):
         d = min(dist(valve, valves[v]) + 1, T - t)
-        res = max(res, dfs(t + d, valves[v], opened | 1 << v) + d*score)
+        res = max(res, dfs(t + d, valves[v], opened | 1 << v) + d * score)
     return res
 
 
--- day16/part2.py
+++ day16/part2.py
@@ -5,7 +5,7 @@ import collections
 data = {
     line.split()[1]: {
         "rate": int(line.split()[4].split("=")[-1][:-1]),
-        "nb": [nb.replace(",", "") for nb in line.split("to")[-1].split()[1:]]
+        "nb": [nb.replace(",", "") for nb in line.split("to")[-1].split()[1:]],
     }
     for line in sys.stdin.read().strip().splitlines()
 }
@@ -27,28 +27,24 @@ def dist(a, b):
 valves = [k for k, v in data.items() if v["rate"]]
 
 T = 26
-
 paths = []
 
 
 def dfs(t=0, valve="AA", opened=0, score=0):
-    global res
     if t == T:
         paths.append((score, opened))
         return
-    s = sum(
-        data[e]["rate"]
-        for i, e in enumerate(valves) if opened & 1 << i
-    )
+    s = sum(data[e]["rate"] for i, e in enumerate(valves) if opened & 1 << i)
     for v in filter(lambda x: not opened & 1 << x, range(len(valves))):
         d = min(dist(valve, valves[v]) + 1, T - t)
-        dfs(t + d, valves[v], opened | 1 << v, score + d*s)
-    paths.append((score + (T-t)*s, opened))
+        dfs(t + d, valves[v], opened | 1 << v, score + d * s)
+    paths.append((score + (T - t) * s, opened))
 
 
 dfs()
 paths.sort(reverse=True)
 res = 0
+
 for s1, o1 in paths:
     for s2, o2 in paths:
         if not o1 & o2:
